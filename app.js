00ab893147dad4a5307f770ce049dffb3dbf2eed
DeekshithVanka <167310767+DeekshithVanka@users.noreply.github.com>
2024-05-04 12:41:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+       
+    const ele=React.createElement("h1",{id:"hele",className:"pele"},[React.createElement("h2",{id:"hele1"},"i need it tobe fresh"),React.createElement("h2",{id:"hele1"},"i need it tobe fresh")]);
+    const root=ReactDOM.createRoot(document.getElementById("root"));
+    console.log(root)
+    root.render(ele)
